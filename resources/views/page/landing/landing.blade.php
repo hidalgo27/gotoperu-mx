755ad9a5bf9349c20e626b2a75ae5ec6a72b48d9
tania <tania.vanessa609@gmail.com>
2021-04-16 18:54:15 +0200

    Update

--- resources/views/page/landing/landing.blade.php
+++ resources/views/page/landing/landing.blade.php
@@ -61,10 +61,23 @@
             </div>
         </div>
     </header>
-    <section class="m-5">
+    <section class="bg-g-dark py-3">
+        <div class="row">
+            <div class="col-3 align-self-center">
+                <h4 class="text-g-yellow text-center">VIAJES SEGUROS</h4>
+            </div>
+            <div class="col-9">
+                <p class="text text-white">
+                    Ofrecemos las mejores condiciones de reserva para este 2021 con nuestro servicio <span class="text-uppercase text-g-yellow">APLAZAMIENTO LIBRE</span>.
+                    Tienen la libertad de posponer su viaje hasta 15 días antes del 31 de diciembre.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="my-5">
         <div class="container">
-            <div class="row pb-4">
-                <div class="col text-center">
+            <div class="row pb-4 justify-content-center">
+                <div class="col-8 text-center">
                     <h2 class="text-uppercase text-g-yellow pb-4">RESERVE CON CONFIANZA</h2>
                     <p class="text">
                         Preparamos nuestros servicios desde el momento que empieza a planificar su viajes hasta finalizar de su experiencia durante su estadía, nuestro maryor compromiso en la seguridad en el viaje.
@@ -72,26 +85,26 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col">
-                    <h3 class="h5 text-center">APLAZAMIENTO LIBRE</h3>
+                <div class="col text-center">
+                    <h3 class="h5">APLAZAMIENTO LIBRE</h3>
                     <p class="text">
                         Los viajes con fechas de salida antes del 31 de diciembre de 2021 se pueden pospone hasta 10 días antes sin cargos por cambio.
                     </p>
                 </div>
-                <div class="col">
-                    <h3 class="h5 text-center">TOURS PRIVADOS</h3>
+                <div class="col text-center">
+                    <h3 class="h5">TOURS PRIVADOS</h3>
                     <p class="text">
                         Podemos ayudarlo a planificar un itinerario de viaje privado. Esto significa que todos los tours serían solo usted y su grupo más el guía.
                     </p>
                 </div>
-                <div class="col">
-                    <h3 class="h5 text-center">FECHAS FLEXIBLES</h3>
+                <div class="col text-center">
+                    <h3 class="h5">FECHAS FLEXIBLES</h3>
                     <p class="text">
                         ¿Se pospone, pero aún no puede comprometerse con fechas específicas? ¡No ha problema! Posponga ahora y elija sus fechas más tarde.
                     </p>
                 </div>
-                <div class="col">
-                    <h3 class="h5 text-center">SIN INFLACIONES</h3>
+                <div class="col text-center">
+                    <h3 class="h5">SIN INFLACIONES</h3>
                     <p class="text">
                         Cubriremos todos los aumentos de precios entre 2020 y 2021. Para viajes pospuestos hasta 2022 o más tarde, se aplican las tasas de inflación estándar.
                     </p>
